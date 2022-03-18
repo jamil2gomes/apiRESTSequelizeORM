a32194d2752d1ae74c53388a8aac5a0528b66e52
ja1000 <jamil.lanister@hotmail.com>
2022-03-18 18:18:23 -0300

    M - relacionamento hasMany e belongsto nas models

--- api/models/niveis.js
+++ api/models/niveis.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
   class Niveis extends Model {
 
     static associate(models) {
-      // define association here
+      Niveis.hasMany(models.Turmas,{foreignKey: 'nivel_id'});
     }
   }
   Niveis.init({
--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -6,7 +6,8 @@ module.exports = (sequelize, DataTypes) => {
   class Pessoas extends Model {
   
     static associate(models) {
-      // define association here
+      Pessoas.hasMany(models.Turmas, {foreignKey: 'docente_id'});
+      Pessoas.hasMany(models.Matriculas, {foreignKey: 'estudante_id'});
     }
   }
 
